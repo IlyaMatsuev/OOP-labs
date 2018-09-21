5c0b42adf537567474b01b3f833bd9dab1aac5b1
IlyaMatsuev <matsuev2000@mail.ru>
2018-09-22 01:39:06 +0300

    pushing third lab

---
+++ thirdLab/Airline.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public partial class  Airline
+    {
+        public uint makeHash(string dest, int flight, char type, weekDays day)
+        {
+            int intRes = dest.Length + flight * 9 - (int)type;
+            if (day > weekDays.thir)
+                intRes *= 54;
+            uint res = (uint)intRes;
+            return res;
+        }
+        
+        
+    }
+}
